19886183f2cce734da4fe11306ae209908cec7f2
Wataly89 <awatton89@gmail.com>
2019-12-02 17:18:19 -0500

    show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :forecasts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "forecasts#index"
+  resources :courses, only: [:index, :show, :edit]
 end
